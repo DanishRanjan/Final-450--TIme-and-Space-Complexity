290ca3b170c34873377f5861af3b0ced03600cff
Danish Ranjan <ranjandanish@gmail.com>
2021-01-17 03:08:06 +0530

    Count Sort

---
+++ countSort.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class countSort {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+			max = Math.max(max, arr[i]);
+			min = Math.min(min, arr[i]);
+		}
+		count(arr, min, max);
+		print(arr);
+	}
+
+	public static void count(int[] arr, int min, int max) {
+		int range = max - min + 1;
+		int farr[] = new int[range];
+
+		for (int i = 0; i < arr.length; i++) {
+			int index = arr[i] - min;
+			farr[index]++;
+		}
+
+		for (int i = 1; i < farr.length; i++) {
+			farr[i] = farr[i] + farr[i - 1];
+		}
+
+		int[] ans = new int[arr.length];
+
+		for (int i = arr.length - 1; i >= 0; i--) {
+			int val = arr[i];
+			int position = farr[val - min];
+			int index = position - 1;
+			ans[index] = val;
+			farr[val - min]--;
+		}
+		for (int i = 0; i < ans.length; i++) {
+			arr[i] = ans[i];
+		}
+
+	}
+
+	public static void print(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
